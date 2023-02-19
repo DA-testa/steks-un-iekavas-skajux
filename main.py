721008e6c1faeb5532f921d98d73f015ef3891dc
221RDB223 <125821576+skajux@users.noreply.github.com>
2023-02-19 21:36:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,10 @@ def find_mismatch(text):
         if next in ")]}":
             # Process closing bracket, write your code here
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
-                print("ir tukss")
                 return i+1
             opening_brackets_stack.pop()
         if opening_brackets_stack:
-            return opening_brackets_stack[-1].position
+            return opening_brackets_stack[0].position
         return "Success"
 
 
