e54a9464f257c045d995fe58f6d23abd8e066ea1
221RDB223 <125821576+skajux@users.noreply.github.com>
2023-02-19 21:41:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ def find_mismatch(text):
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
-        if opening_brackets_stack:
-            return opening_brackets_stack[0].position
-        return "Success"
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    return "Success"
 
 
 def main():
@@ -34,4 +34,4 @@ def main():
     print(mismatch)
 
 
-    main()
+main()
