0b5ac6bd40ed971b146907d2ddd61860fcd82322
221RDB223 <125821576+skajux@users.noreply.github.com>
2023-02-19 21:32:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,18 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next,i+1))
+            
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                print("ir tukss")
+                return i+1
+            opening_brackets_stack.pop()
+        if opening_brackets_stack:
+            return opening_brackets_stack[-1].position
+        return "Success"
 
 
 def main():
@@ -27,5 +34,5 @@ def main():
     # Printing answer, write your code here
 
 
-if __name__ == "__main__":
+
     main()
