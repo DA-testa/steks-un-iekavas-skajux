27df9103c70d343a02c8411988a608d7383722ed
221RDB223 <125821576+skajux@users.noreply.github.com>
2023-02-19 21:45:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,10 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            opening_brackets_stack.append(Bracket(next,i+1))
-            
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
+            # Process opening bracket
+            opening_brackets_stack.append(Bracket(next, i+1))
+        elif next in ")]}":
+            # Process closing bracket
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
@@ -30,8 +28,47 @@ def find_mismatch(text):
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-    print(mismatch)
+    # Printing answer
+    if mismatch == "Success":
+        print(mismatch)
+    else:
+        print(mismatch)
+
+
+main()
+from collections import namedtuple
+
+Bracket = namedtuple("Bracket", ["char", "position"])
+
+
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
+
+
+def find_mismatch(text):
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next in "([{":
+            # Process opening bracket
+            opening_brackets_stack.append(Bracket(next, i+1))
+        elif next in ")]}":
+            # Process closing bracket
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i+1
+            opening_brackets_stack.pop()
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    return "Success"
+
+
+def main():
+    text = input()
+    mismatch = find_mismatch(text)
+    # Printing answer
+    if mismatch == "Success":
+        print(mismatch)
+    else:
+        print(mismatch)
 
 
 main()
